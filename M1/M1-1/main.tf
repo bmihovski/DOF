1951179a2ddf1070aa96a2e64aeecf8c02baf1e6
bmihovski <bmihovski@live.com>
2018-12-13 23:21:16 +0200

    m1-1 completed

--- M1/M1-1/main.tf
+++ M1/M1-1/main.tf
@@ -1,4 +1,19 @@
 provider "aws" {
-    access_key = ''
-    secret_key = ''
+  access_key = "AKIAIKZITS2WRUFTBIYA"
+  secret_key = ""
+  region     = "eu-central-1"
+}
+
+resource "aws_instance" "M1-1" {
+  ami           = "ami-0bdf93799014acdc4"
+  instance_type = "t2.micro"
+  key_name      = "terraform-key"
+}
+
+output "Public IP" {
+  value = "${aws_instance.M1-1.public_ip}"
+}
+
+output "Public DNS" {
+  value = "${aws_instance.M1-1.public_dns}"
 }
